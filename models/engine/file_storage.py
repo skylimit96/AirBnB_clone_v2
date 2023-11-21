511546e2208838df2e4224b5d0d1c1c3c2d6643d
Amine Sehlaoui <a.sehlaoui96@gmail.com>
2023-11-21 12:47:21 -0800

    file storage file updated to add delete feature

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -48,3 +48,8 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        if obj:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            del self.__objects[key]
