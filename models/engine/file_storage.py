b6bf86625ce5ddf0fdfd2d7ad0b0063589dd2739
Amine Sehlaoui <a.sehlaoui96@gmail.com>
2023-11-21 12:53:15 -0800

    file storage updated

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -66,3 +66,6 @@ class FileStorage:
         if obj:
             key = "{}.{}".format(type(obj).__name__, obj.id)
             del self.__objects[key]
+
+    def close(self):
+        self.reload()
